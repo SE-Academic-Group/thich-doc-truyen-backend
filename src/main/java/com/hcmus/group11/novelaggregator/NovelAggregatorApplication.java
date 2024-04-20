5cf178e3b365d268867e4fd01bf87eb62cbdbe37
21120521 <nguyenphucphat111999@gmail.com>
2024-04-20 12:10:17 +0700

    chore: init project

---
+++ src/main/java/com/hcmus/group11/novelaggregator/NovelAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.hcmus.group11.novelaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcmus/group11/novelaggregator/NovelAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmus.group11.novelaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
