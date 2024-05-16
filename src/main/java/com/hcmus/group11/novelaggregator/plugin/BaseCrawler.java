4579b8e75c1197dd7cac7162153789a499983501
Flashback054 <flashback054@gmail.com>
2024-05-16 16:41:14 +0700

    feat: complete pagination metadata

--- src/main/java/com/hcmus/group11/novelaggregator/plugin/BaseCrawler.java
+++ src/main/java/com/hcmus/group11/novelaggregator/plugin/BaseCrawler.java
@@ -1,6 +1,8 @@
 package com.hcmus.group11.novelaggregator.plugin;
 
 import com.hcmus.group11.novelaggregator.type.NovelSearchResult;
+import com.hcmus.group11.novelaggregator.type.ResponseMetadata;
+import com.hcmus.group11.novelaggregator.util.RequestAttributeUtil;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,7 +32,13 @@ public abstract class BaseCrawler implements INovelPlugin {
     public List<NovelSearchResult> search(String keyword, Integer page) {
         String searchUrl = buildSearchUrl(keyword, page);
         Document html = getHtml(searchUrl);
+
         List<NovelSearchResult> novelSearchResults = parseSearchHTML(html);
+        ResponseMetadata metadata = parseSearchMetadata(html);
+        metadata.addMetadataValue("currentPage", page);
+        metadata.addMetadataValue("pluginName", pluginName);
+
+        RequestAttributeUtil.setAttribute("metadata", metadata);
         return novelSearchResults;
     }
 
@@ -46,4 +54,8 @@ public abstract class BaseCrawler implements INovelPlugin {
     protected abstract String buildSearchUrl(String keyword, Integer page);
 
     protected abstract List<NovelSearchResult> parseSearchHTML(Document html);
+
+    protected ResponseMetadata parseSearchMetadata(Document html) {
+        return null;
+    }
 }
